ef3cfc1f9032a1ce5844aded28f5fa6c9200ee3e
RomaShpylovyi <44481626+ShpylovyiRoman@users.noreply.github.com>
2019-06-05 01:05:48 +0300

    Update baseOfGraph.js

--- baseOfGraph.js
+++ baseOfGraph.js
@@ -11,10 +11,8 @@ function inputDataChecker() {
   );
   inputNumber = Number(inputNumber);
   if (typeof inputNumber === 'number' && inputNumber > 1) return inputNumber;
-  else {
-    console.log('Invalid input, please enter number above one. ');
-    return inputDataChecker();
-  }
+  console.log('Invalid input, please enter number above one. ');
+  return inputDataChecker();
 }
 
 //creating class of edges
@@ -86,75 +84,18 @@ function algorithmKraskal(weightMatrix) {
   return baseMatrix;
 }
 
-//Function for finding lead time
-function timeTester(matrix) {
-  const start = new Date().getTime();
-  const baseOfGraph = algorithmKraskal(matrix);
-  const end = new Date().getTime();
-
-  const runningTime = end - start;
-  return [baseOfGraph, runningTime];
-}
 
 //Usage
 const dimensionMatrix = inputDataChecker();
-const dimensionBigMatrix = dimensionMatrix * 2;
-
 const matrixGraph = new Matrix(dimensionMatrix);
 matrixGraph
   .fillWeight()
   .symetric()
   .zerosDiagonal();
-const secondMatrix = new Matrix(dimensionMatrix);
-secondMatrix
-  .fillWeight()
-  .symetric()
-  .zerosDiagonal();
 
-const bigMatrixGraph = new Matrix(dimensionBigMatrix);
-bigMatrixGraph
-  .fillWeight()
-  .symetric()
-  .zerosDiagonal();
-const bigMatrixSecondGraph = new Matrix(dimensionBigMatrix);
-bigMatrixSecondGraph
-  .fillWeight()
-  .symetric()
-  .zerosDiagonal();
 
-// console.log('First matrix: \n')
-// console.log(firstMatrixGraph.matrix);
-// console.log('Second matrix: \n')
-// console.log(secondMatrix.matrix);
-// console.log('First big matrix: \n')
-// console.log(bigMatrixGraph.matrix);
-// console.log('Second big matrix: \n')
-// console.log(bigMatrixSecondGraph.matrix);
+console.log(matrixGraph.matrix)
+const baseOfGraph = algorithmKraskal(matrixGraph.matrix);
+console.log(baseOfGraph.matrix)
 
-//Finding lead time
-let baseMatrix,
-    firstTime,
-    secondBaseMatrix,
-    secondTime,
-    bigBaseMatrix,
-    bigMatrixTime,
-    secondBaseBigMatrix,
-    bigMatrixSecondTime;
-
-[baseMatrix, firstTime] = timeTester(matrixGraph.matrix);
-// console.log(baseMatrix.matrix);
-console.log(`Running time for first graph: ${firstTime}ms`);
-
-[secondBaseMatrix, secondTime] = timeTester(secondMatrix.matrix);
-// console.log(secondBaseMatrix.matrix);
-console.log(`Running time for second graph: ${secondTime}ms`);
-
-[bigBaseMatrix, bigMatrixTime] = timeTester(bigMatrixGraph.matrix);
-// console.log(bigBaseMatrix.matrix);
-console.log(`Running time for big graph: ${bigMatrixTime}ms`);
-
-[secondBaseBigMatrix, bigMatrixSecondTime] = timeTester(
-  bigMatrixSecondGraph.matrix
-);
-// console.log(secondBaseBigMatrix.matrix);
-console.log(`Running time for second big graph: ${bigMatrixSecondTime}ms`);
+module.exports = algorithmKraskal;
